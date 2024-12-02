37c90a670e495ed3a1eea863fa1630d1544a88a8
NIhad <nihadahmedzade@gmail.com>
2024-12-02 18:22:14 +0400

    classroom

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const navbar_left_menu = document.querySelector(".navbar_left_menu")
+const sidebar = document.querySelector(".sidebar")
+navbar_left_menu.addEventListener("click",function () {
+    sidebar.classList.toggle("menu_click")
+   
+})
